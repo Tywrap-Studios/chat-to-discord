ed2d92f3233cbacf340e6b97211bad8a81962af4
TywrapStudios <tywrap.studio@gmail.com>
2025-05-30 12:51:48 +0200

    Spark AsyncWorldInfoProvider mixin fix
    Took 23 minutes

--- fabric/src/main/java/org/tywrapstudios/ctd/mixin/AsyncWorldInfoProviderMixin.java
+++ fabric/src/main/java/org/tywrapstudios/ctd/mixin/AsyncWorldInfoProviderMixin.java
@@ -2,13 +2,13 @@ package org.tywrapstudios.ctd.mixin;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import me.lucko.spark.common.platform.world.AsyncWorldInfoProvider;
-import org.tywrapstudios.ctd.compat.Spark;
 import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.tywrapstudios.ctd.compat.Spark;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeoutException;
