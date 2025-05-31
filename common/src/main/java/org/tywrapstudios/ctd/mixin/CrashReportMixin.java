631768cfba57e417dd13b64b85e9187d50aefbdd
TywrapStudios <tywrap.studio@gmail.com>
2025-05-31 16:51:19 +0200

    CrashReportMixin fix
    Took 26 seconds

--- common/src/main/java/org/tywrapstudios/ctd/mixin/CrashReportMixin.java
+++ common/src/main/java/org/tywrapstudios/ctd/mixin/CrashReportMixin.java
@@ -24,7 +24,7 @@ public abstract class CrashReportMixin {
     @Shadow @Final private static Logger LOGGER;
 
     @Inject(method = "saveToFile(Ljava/nio/file/Path;Lnet/minecraft/ReportType;Ljava/util/List;)Z",
-            at = @At(value = "HEAD"))
+            at = @At(value = "INVOKE", target = "Ljava/io/Writer;write(Ljava/lang/String;)V", shift = At.Shift.AFTER))
     private void ctd$sendWebhookOnCrash(Path path, ReportType type, List<String> links, CallbackInfoReturnable<Boolean> cir) {
         String cause = getExceptionMessage();
         try {
