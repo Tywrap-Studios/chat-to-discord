81a289a22b36c50414d4a00127fbb9faf26e6dc4
Tiazzz <163735662+ItsTiazzz@users.noreply.github.com>
2025-07-19 21:08:06 +0200

    Holy Gradle
    Took 7 hours 1 minute

--- krafter/build.gradle.kts
+++ krafter/build.gradle.kts
@@ -1,28 +1,93 @@
+import dev.kordex.gradle.plugins.kordex.DataCollection
+
 plugins {
-    kotlin("jvm") version "2.1.21"
-    id("dev.kordex.gradle.kordex") version "1.6.1"
+    distribution
+
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.serialization)
+
+    alias(libs.plugins.detekt)
+
+    alias(libs.plugins.kordex.plugin)
+    alias(libs.plugins.ksp.plugin)
 }
 
-repositories {
+allprojects {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        google()
 
-}
-
-dependencies {
-    implementation(project(":common"))
-    implementation(libs.kotlin.stdlib)
-//    implementation(libs.slf4j)
-}
-
-kordEx {
-    bot {
-        mainClass = "org.tywrapstudios.krafter.AppKt"
+        maven {
+            name = "KordEx (Snapshots)"
+            url = uri("https://snapshots-repo.kordex.dev")
+        }
+        maven {
+            name = "KordEx (Releases)"
+            url = uri("https://releases-repo.kordex.dev")
+        }
+        maven {
+            name = "QuiltMC (Snapshots)"
+            url = uri("https://maven.quiltmc.org/repository/snapshot/")
+        }
+        maven {
+            name = "JitPack"
+            url = uri("https://jitpack.io")
+        }
     }
 }
 
-tasks.test {
-    useJUnitPlatform()
+dependencies {
+    detektPlugins(libs.detekt)
+
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.kx.ser)
+
+    // Logging dependencies
+    implementation(libs.groovy)
+    implementation(libs.jansi)
+    implementation(libs.logback)
+    implementation(libs.logback.groovy)
+    implementation(libs.logging)
+
+    // Other dependencies
+    implementation(libs.bbapi)
+    // We need 1.1.0 Cozy Modules but Gradle is a bitch
+    implementation(libs.bundles.cozy.modules)
+
+    compileOnly(project(":common"))
 }
 
-kotlin {
-    jvmToolchain(21)
+// Configure distributions plugin
+distributions {
+    main {
+        distributionBaseName = project.name
+
+        contents {
+            from("LICENSE")
+        }
+    }
+}
+
+kordEx {
+    kordExVersion = libs.versions.kordex.asProvider()
+
+    bot {
+        // See https://docs.kordex.dev/data-collection.html
+        dataCollection(DataCollection.Standard)
+
+        mainClass = "org.tywrapstudios.krafter.AppKt"
+    }
+
+    module("pluralkit")
+    module("func-phishing")
+    module("func-mappings")
+    // Currently unavailable
+//    module("func-minecraft")
+}
+
+detekt {
+    buildUponDefaultConfig = true
+
+    config.from(rootProject.files("detekt.yml"))
 }
