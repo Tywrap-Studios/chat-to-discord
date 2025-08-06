eb3c607d51b37bcc0c9703cd0e1b6e28060ebf8e
Tiazzz <163735662+ItsTiazzz@users.noreply.github.com>
2025-08-06 22:00:02 +0200

    Update BBAPI to 2.1.0

--- krafter/build.gradle.kts
+++ krafter/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     implementation(libs.logging)
 
     // Other dependencies
-    implementation(libs.bbapi)
+    api(libs.bbapi)
     // We need 1.1.0 Cozy Modules but Gradle is in a love-hate relationship with Quilt's Maven
     implementation(libs.bundles.cozy.modules)
     // I guess we're going the way of the flatDir *sigh*
